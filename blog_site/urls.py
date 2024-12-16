39259c6a048d3314bd4f5e30429bb94941adb803
Xumoyiddin014 <xtuxtamirzayev@gmail.com>
2024-12-16 20:38:37 +0500

    add models, admin, views, templates

---
+++ blog_site/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog_site'
+
+urlpatterns = [
+    path('', views.PostListView(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail(), name='post_detail')
+]
